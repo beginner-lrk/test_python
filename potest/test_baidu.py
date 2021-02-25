2cccec7ac60ba66f0a75d35c549452ba843246a3
Administrator <429564836@qq.com>
2021-02-25 17:30:53 +0800

    百度搜索PO模式流程,批量执行用例

--- potest/test_baidu.py
+++ potest/test_baidu.py
@@ -5,6 +5,9 @@ from selenium import webdriver
 from baidu_page import Baidu_page
 import unittest
 from time import sleep
+from parameterized import parameterized
+def get_data():
+    return [("测试","测试_百度搜索"),("selenium","测试_百度搜索")]
 
 class TestBaidu(unittest.TestCase):
     @classmethod
@@ -13,11 +16,11 @@ class TestBaidu(unittest.TestCase):
         self.url = "http://www.baidu.com"
     def tearDown(self):
         self.driver.quit()
-
-    def test_baidu_search_case1(self):
+    @parameterized.expand(get_data())
+    def test_baidu_search_case1(self,port,expect):
         page =Baidu_page(self.driver)
         page.open(self.url)
-        page.search_input("测试")
+        page.search_input(port)
         page.search_button()
         sleep(3)
-        self.assertEqual(page.get_title(),"测试_百度搜索")
+        self.assertEqual(page.get_title(),expect)
