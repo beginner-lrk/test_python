0d32ba2c603fdccbdec3f5271c3e4e09be751267
Administrator <429564836@qq.com>
2021-02-26 16:30:02 +0800

    新增数据库操作

---
+++ common/sql_insert.py
@@ -0,0 +1,28 @@
+#! /usr/bin/pythpyon
+# -*- coding: UTF-8 -*-
+# @Time : 2021/2/26 14:53
+import pymysql
+#step1：数据库链接
+conn = pymysql.Connect(host="192.1.3.149",
+                       port=3306,
+                       database="woa_usercenter",
+                       user="woa",
+                       password="Hzlh2020s",
+                          )
+#step2:创建游标对象
+con = conn.cursor()
+#step3:sql语句并发送
+try:
+    sql1 = "INSERT INTO `uc_tuserbase` VALUES ('fdb06846-5f20-42cd-81f6-4ce280791111', 402, NULL, NULL, 'mbl1830509761', '477255324ffe401128f11a556c6263c8', 'Helloworld', NULL, 3, NULL, 1, NULL, NULL, NULL, NULL, NULL, 1, '', NULL, '2020-04-01 13:43:43', 9, 0, NULL, 'syssteplinkb', '2020-03-17 13:16:59', NULL, NULL, NULL, 1, 0, '10033', NULL, NULL, -1, 0)"
+    sql2 = "INSERT INTO `uc_tuserbase` VALUES ( 'Helloworld', NULL, 3, NULL, 1, NULL, NULL, NULL, NULL, NULL, 1, '', NULL, '2020-04-01 13:43:43', 9, 0, NULL, 'syssteplinkb', '2020-03-17 13:16:59', NULL, NULL, NULL, 1, 0, '10033', NULL, NULL, -1, 0)"
+    con.execute(sql1)
+    con.execute(sql2)
+    # 如果上述两条 SQL 语句执行正常,手动提交
+    conn.commit()
+except Exception as e:
+    print(e)
+    #数据库操作回滚
+    conn.rollback()
+finally:
+    conn.close()
+    con.close()
---
+++ common/sql_select.py
@@ -0,0 +1,39 @@
+#! /usr/bin/python
+# -*- coding: UTF-8 -*-
+# @Time : 2021/2/26 10:41
+import pymysql
+
+#step1:创建连接，调用connect函数
+conn = pymysql.Connect(host="192.1.3.149",
+                       port=3306,
+                       database="woa_scrm",
+                       user="woa",
+                       password="Hzlh2020s",
+                       autocommit=True
+                       )
+
+#step2:创建游标对象，用来给数据库发送sql语句，并执行
+cursor = conn.cursor()
+#查看数据库是否连接成功
+cnn =conn.open
+if cnn == True:
+    print("数据库连接成功")
+else:
+    print("数据库连接成功")
+#step3:发送sql语句
+sql = "SELECT * FROM busi_merchant_saler where ORG_ID=10092;"
+#step4:执行sql
+cursor.execute(sql)
+#step5:获取查询到的数据，默认元组所以要for in 来获取具体数值
+salers = cursor.fetchall()
+for saler in salers:
+    print("CUSTOMER_ID:",saler[0])
+    print("PHONE:",saler[7])
+'''
+默认情况下，新增/修改/删除操作不会提交到数据库
+# 方案1:手动提交 conn.commit() 
+# 方案2:设置自动提交 创建 conn 时，设置参数 autocommit = True (默认是 False)
+'''
+#step6:资源释放
+cursor.close()
+conn.close()
